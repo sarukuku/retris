8948903556b83f9c9e332d96a366132cda270c2e
Akos Szokodi <szokodiakos@gmail.com>
2018-10-19 14:00:59 +0200

    Remove css prefix

--- src/views/loading.tsx
+++ src/views/loading.tsx
@@ -10,33 +10,9 @@ export class Loading extends Component {
             position: fixed;
             top: 50%;
             left: 50%;
-            width: 2 0%;
+            width: 20%;
             margin: -10% 0 0 -10%;
-            -webkit-animation: spin 3s linear infinite;
-            -moz-animation: spin 3s linear infinite;
             animation: spin 3s linear infinite;
-          }
-
-          @-moz-keyframes spin {
-            49%,
-            51% {
-              -moz-transform: scale(-1, 1);
-            }
-
-            100% {
-              -moz-transform: scale(1, 1);
-            }
-          }
-
-          @-webkit-keyframes spin {
-            49%,
-            51% {
-              -webkit-transform: scale(-1, 1);
-            }
-
-            100% {
-              -webkit-transform: scale(1, 1);
-            }
           }
 
           @keyframes spin {
