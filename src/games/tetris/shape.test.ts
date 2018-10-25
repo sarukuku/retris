d7bb669a1657cef981efbf94d00561a8ebb3f48a
Akos Szokodi <szokodiakos@gmail.com>
2018-10-25 11:31:02 +0200

    add shapes

---
+++ src/games/tetris/shape.test.ts
@@ -0,0 +1,213 @@
+import { Shape } from "./shape"
+
+const _ = undefined
+const color = "red"
+const x = { color }
+
+const tests = [
+  {
+    name: "I shape",
+    createShape: Shape.createIShape,
+    initial: [
+      [_, _, x, _], //
+      [_, _, x, _],
+      [_, _, x, _],
+      [_, _, x, _],
+    ],
+    rotateOnce: [
+      [_, _, _, _], //
+      [_, _, _, _],
+      [x, x, x, x],
+      [_, _, _, _],
+    ],
+    rotateTwice: [
+      [_, _, x, _], //
+      [_, _, x, _],
+      [_, _, x, _],
+      [_, _, x, _],
+    ],
+    rotateThrice: [
+      [_, _, _, _], //
+      [_, _, _, _],
+      [x, x, x, x],
+      [_, _, _, _],
+    ],
+  },
+  {
+    name: "J shape",
+    createShape: Shape.createJShape,
+    initial: [
+      [_, x, _], //
+      [_, x, _],
+      [x, x, _],
+    ],
+    rotateOnce: [
+      [x, _, _], //
+      [x, x, x],
+      [_, _, _],
+    ],
+    rotateTwice: [
+      [_, x, x], //
+      [_, x, _],
+      [_, x, _],
+    ],
+    rotateThrice: [
+      [_, _, _], //
+      [x, x, x],
+      [_, _, x],
+    ],
+  },
+  {
+    name: "L shape",
+    createShape: Shape.createLShape,
+    initial: [
+      [_, x, _], //
+      [_, x, _],
+      [_, x, x],
+    ],
+    rotateOnce: [
+      [_, _, _], //
+      [x, x, x],
+      [x, _, _],
+    ],
+    rotateTwice: [
+      [x, x, _], //
+      [_, x, _],
+      [_, x, _],
+    ],
+    rotateThrice: [
+      [_, _, x], //
+      [x, x, x],
+      [_, _, _],
+    ],
+  },
+  {
+    name: "O shape",
+    createShape: Shape.createOShape,
+    initial: [
+      [x, x], //
+      [x, x],
+    ],
+    rotateOnce: [
+      [x, x], //
+      [x, x],
+    ],
+    rotateTwice: [
+      [x, x], //
+      [x, x],
+    ],
+    rotateThrice: [
+      [x, x], //
+      [x, x],
+    ],
+  },
+  {
+    name: "S shape",
+    createShape: Shape.createSShape,
+    initial: [
+      [_, x, x], //
+      [x, x, _],
+      [_, _, _],
+    ],
+    rotateOnce: [
+      [_, x, _], //
+      [_, x, x],
+      [_, _, x],
+    ],
+    rotateTwice: [
+      [_, _, _], //
+      [_, x, x],
+      [x, x, _],
+    ],
+    rotateThrice: [
+      [x, _, _], //
+      [x, x, _],
+      [_, x, _],
+    ],
+  },
+  {
+    name: "T shape",
+    createShape: Shape.createTShape,
+    initial: [
+      [_, _, _], //
+      [x, x, x],
+      [_, x, _],
+    ],
+    rotateOnce: [
+      [_, x, _], //
+      [x, x, _],
+      [_, x, _],
+    ],
+    rotateTwice: [
+      [_, x, _], //
+      [x, x, x],
+      [_, _, _],
+    ],
+    rotateThrice: [
+      [_, x, _], //
+      [_, x, x],
+      [_, x, _],
+    ],
+  },
+  {
+    name: "Z shape",
+    createShape: Shape.createZShape,
+    initial: [
+      [x, x, _], //
+      [_, x, x],
+      [_, _, _],
+    ],
+    rotateOnce: [
+      [_, _, x], //
+      [_, x, x],
+      [_, x, _],
+    ],
+    rotateTwice: [
+      [_, _, _], //
+      [x, x, _],
+      [_, x, x],
+    ],
+    rotateThrice: [
+      [_, x, _], //
+      [x, x, _],
+      [x, _, _],
+    ],
+  },
+]
+
+tests.map(
+  ({ name, createShape, initial, rotateOnce, rotateTwice, rotateThrice }) => {
+    test(`${name} initial`, () => {
+      const shape = createShape(color)
+
+      expect(shape.draw()).toEqual(initial)
+    })
+
+    test(`${name} rotateOnce`, () => {
+      const shape = createShape(color)
+
+      shape.rotate()
+
+      expect(shape.draw()).toEqual(rotateOnce)
+    })
+
+    test(`${name} rotateTwice`, () => {
+      const shape = createShape(color)
+
+      shape.rotate()
+      shape.rotate()
+
+      expect(shape.draw()).toEqual(rotateTwice)
+    })
+
+    test(`${name} rotateThrice`, () => {
+      const shape = createShape(color)
+
+      shape.rotate()
+      shape.rotate()
+      shape.rotate()
+
+      expect(shape.draw()).toEqual(rotateThrice)
+    })
+  },
+)
---
+++ src/games/tetris/shape.ts
@@ -0,0 +1,89 @@
+import { transpose } from "ramda"
+import { Matrix } from "./matrix"
+
+const _ = undefined
+
+export class Shape {
+  protected constructor(protected matrix: Matrix) {}
+
+  draw() {
+    return this.matrix
+  }
+
+  rotate() {
+    this.matrix = Shape.reverseColumns(transpose(this.matrix))
+  }
+
+  private static reverseColumns(matrix: Matrix): Matrix {
+    return matrix.map(row => row.reverse())
+  }
+
+  static createIShape(color: string) {
+    const x = { color }
+    return new IShape([
+      [_, _, x, _], //
+      [_, _, x, _],
+      [_, _, x, _],
+      [_, _, x, _],
+    ])
+  }
+
+  static createJShape(color: string) {
+    const x = { color }
+    return new Shape([
+      [_, x, _], //
+      [_, x, _],
+      [x, x, _],
+    ])
+  }
+
+  static createLShape(color: string) {
+    const x = { color }
+    return new Shape([
+      [_, x, _], //
+      [_, x, _],
+      [_, x, x],
+    ])
+  }
+
+  static createOShape(color: string) {
+    const x = { color }
+    return new Shape([
+      [x, x], //
+      [x, x],
+    ])
+  }
+
+  static createSShape(color: string) {
+    const x = { color }
+    return new Shape([
+      [_, x, x], //
+      [x, x, _],
+      [_, _, _],
+    ])
+  }
+
+  static createTShape(color: string) {
+    const x = { color }
+    return new Shape([
+      [_, _, _], //
+      [x, x, x],
+      [_, x, _],
+    ])
+  }
+
+  static createZShape(color: string) {
+    const x = { color }
+    return new Shape([
+      [x, x, _], //
+      [_, x, x],
+      [_, _, _],
+    ])
+  }
+}
+
+class IShape extends Shape {
+  rotate() {
+    this.matrix = transpose(this.matrix)
+  }
+}
