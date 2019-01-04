ad8f06265c2729a198a89ba0eb8c58cb007da880
Joonas Salovaara <joonas.salovaara@gmail.com>
2019-01-04 14:53:44 +0200

    [ref #69] Add bugsnack error tracking
    This closes #69.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,12 @@ import { Translations } from "../i18n/default-translations"
 import { createTranslate } from "../i18n/translate"
 import { colors } from "../styles/colors"
 import { fonts, withFallback } from "../styles/fonts"
+import bugsnag from '@bugsnag/js'
+import bugsnagReact from '@bugsnag/plugin-react'
+
+const bugsnagClient = bugsnag('d6bc936b6e415dd8ccbf63d75dbd5641')
+bugsnagClient.use(bugsnagReact, React)
+const ErrorBoundary = bugsnagClient.getPlugin('react')
 
 interface RetrisProps {
   translations: Translations
@@ -33,41 +39,43 @@ class Retris extends App<RetrisProps> {
     const analytics = new GoogleAnalytics(clientConfig.googleAnalytics)
 
     return (
-      <AnalyticsContext.Provider value={analytics}>
-        <TranslationContext.Provider value={createTranslate(translations)}>
-          <Container>
-            <Head>
-              <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, shrink-to-fit=no"
-              />
-            </Head>
-            <Component {...pageProps} />
-            <style global jsx>{`
-              html {
-                box-sizing: border-box;
-              }
+      <ErrorBoundary>
+        <AnalyticsContext.Provider value={analytics}>
+          <TranslationContext.Provider value={createTranslate(translations)}>
+            <Container>
+              <Head>
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, shrink-to-fit=no"
+                />
+              </Head>
+              <Component {...pageProps} />
+              <style global jsx>{`
+                html {
+                  box-sizing: border-box;
+                }
 
-              *,
-              *:before,
-              *:after {
-                box-sizing: inherit;
-              }
+                *,
+                *:before,
+                *:after {
+                  box-sizing: inherit;
+                }
 
-              body {
-                overflow-y: hidden;
-                font-family: ${withFallback(fonts.JUNGKA)};
-                color: ${colors.WHITE};
-                background-color: ${colors.BLACK};
-                line-height: 1.35;
-                width: 100%;
-                height: 100%;
-                position: fixed;
-              }
-            `}</style>
-          </Container>
-        </TranslationContext.Provider>
-      </AnalyticsContext.Provider>
+                body {
+                  overflow-y: hidden;
+                  font-family: ${withFallback(fonts.JUNGKA)};
+                  color: ${colors.WHITE};
+                  background-color: ${colors.BLACK};
+                  line-height: 1.35;
+                  width: 100%;
+                  height: 100%;
+                  position: fixed;
+                }
+              `}</style>
+            </Container>
+          </TranslationContext.Provider>
+        </AnalyticsContext.Provider>
+      </ErrorBoundary>
     )
   }
 }
